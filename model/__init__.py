e099fd4573ce590ad45363bd6747f40c8beeb6f3
kshku <1211shree@gmail.com>
2026-08-04 21:30:06 +0530

    feat: expose Classifier, Localizer, ModelRegistry at model package level

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,6 @@
 # Swaraaha - ML Models
 # Shared model code for both desktop app and backend
+
+from model.registry import Classifier, Localizer, ModelRegistry
+
+__all__ = ["Classifier", "Localizer", "ModelRegistry"]
