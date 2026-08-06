060698ffb3835b7080762c05e6afb4b7bdebb229
Srinivas Hegde M <hegdesrinivasm@gmail.com>
2026-08-06 15:26:45 +0530

    feat: add language selection dialog

---
+++ app/tests/test_language_dialog.py
@@ -0,0 +1,21 @@
+from PySide6.QtWidgets import QDialog
+
+from app.ui.language_dialog import LanguageDialog
+
+
+def test_language_dialog_has_three_buttons(qapp):
+    dialog = LanguageDialog()
+    assert set(dialog._buttons.keys()) == {"english", "kannada", "hindi"}
+
+
+def test_language_dialog_prehighlights_current(qapp):
+    dialog = LanguageDialog("kannada")
+    assert dialog._buttons["kannada"].property("cssClass") == "nav_btn_active"
+    assert dialog._buttons["english"].property("cssClass") is None
+
+
+def test_language_dialog_click_returns_code(qapp):
+    dialog = LanguageDialog()
+    dialog._buttons["hindi"].click()
+    assert dialog.selected() == "hindi"
+    assert dialog.result() == QDialog.DialogCode.Accepted
